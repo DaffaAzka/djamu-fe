6991ad6b6871c0d7131bec21d55dda3d796ac892
Azka <azkadaiki26@gmail.com>
2026-02-06 09:21:58 +0700

    feat: navbar

--- app/components/layouts/guest_layout.tsx
+++ app/components/layouts/guest_layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Navbar from "../ui/navbar";
 
 export default function GuestLayout() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
